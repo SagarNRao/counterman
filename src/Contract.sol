4ec0a30a39d5a4449a9b696422a2c4ac5cda46c3
SagarNRao <sagarnrao154@gmail.com>
2024-06-21 12:51:33 +0530

    second commit

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,6 +1,34 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.19;
 
-contract Contract {
-    constructor() {}
+contract counterman {
+    uint256 public counter = 0;
+    address public owner;
+    constructor() {
+        counter = 0;
+        owner = msg.sender;
+    }
+
+    modifier onlyowner() {
+        require(msg.sender == owner, "Only the owner can call this function");
+        _;
+    }
+
+    function increment() public{
+        counter++;
+    }
+
+    function decrement() public{
+        require(counter > 0, "Can't be negative");
+        counter--;
+    }
+
+    function reset() public onlyowner{
+        counter = 0;
+    }
+
+    function getcount() public view returns (uint256)
+    {
+        return counter;
+    }
 }
